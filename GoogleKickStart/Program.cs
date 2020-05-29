08a9748315cdda3261aad88cc9492ddd00825e7e
Gregory Levit <levitg@gmail.com>
2020-05-30 00:15:12 +0300

    Add project files.

---
+++ GoogleKickStart/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GoogleKickStart
+{
+    // On google servers: Mono JIT 4.6.2 (package: mono-devel)
+    //  mcs -optimize+ -r:System.Numerics.dll -out:Solution.exe Solution.cs
+
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
